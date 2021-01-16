43e37d2d9d86586fbadeb217b01cbea924728ce3
LaurieSue <pink.bee.me@outlook.com>
2021-01-15 23:57:16 -0600

    Add project files.

---
+++ AutoInsuranceConnectionApp/Controllers/QuoteController.cs
@@ -0,0 +1,26 @@
+﻿using AutoInsuranceConnectionApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoInsuranceConnectionApp.Controllers
+{
+    public class QuoteController : Controller
+    {
+        // GET: Quote
+        public ActionResult GetInsureeData()
+        {
+            Insuree quoteData = new Insuree();
+            return View(quoteData);
+        }
+
+
+      //  public ActionResult CalculateQuote()
+      //  {
+
+      //  }
+
+    }
+}
